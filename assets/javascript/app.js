688574e4523853741c0f6232f118bf12dbd5a1e6
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-08-27 09:29:55 -0500

    pseudo code and started js

---
+++ assets/javascript/app.js
@@ -0,0 +1,131 @@
+$(function() {
+ 
+  // Questions object
+  var Questions = {
+    1: {
+      question_text: 'Is this question one?',
+      answer_one: 'Answer One',
+      answer_two: 'Answer Two',
+      answer_three: 'Answer Three',
+      answer_four: 'Answer Four',
+      correct_answer: 'Answer One'
+    },
+    2: {
+      question_text: 'Is this question two?',
+      answer_one: 'Answer One',
+      answer_two: 'Answer Two',
+      answer_three: 'Answer Three',
+      answer_four: 'Answer Four',
+      correct_answer: 'Answer Two'
+    }
+  };
+
+
+  // set global vars
+  var $question = $('.Question'),
+      $radio = $('.radio'),
+      $counter = 1,
+      $totalQuestions = 0;
+
+
+  // set total # of questions in var: $totalQuestions
+  for(var question in Questions) {
+    $totalQuestions++; // number of questions
+  }
+
+
+
+
+
+
+  // game object
+  var game = {
+
+    // set vars
+    questionContainer: $('.Question'),
+    radio: $('input[type="radio"]'),
+
+
+    
+    // init
+    init: function() {
+
+      // get a question
+      this.getQuestion();
+      
+
+    },
+
+    // radio button template
+    radioTemplate: function(answer) {
+      return  '<label class="radio">' +
+              '  <input type="radio" name="question_' + $counter + '" value="' + answer + '" />' +
+              '  <span class="radio__label">' + answer + '</span>' +
+              '</label>';
+    },
+
+    getQuestion: function() {
+      console.log($counter);
+      this.questionContainer.find('.Question__text').html(Questions[$counter].question_text);
+      this.questionContainer.find('.Question__answers form')
+        .append(this.radioTemplate(Questions[$counter].answer_one))
+        .append(this.radioTemplate(Questions[$counter].answer_two))
+        .append(this.radioTemplate(Questions[$counter].answer_three))
+        .append(this.radioTemplate(Questions[$counter].answer_four));
+
+      // check question
+      this.checkQuestion();
+
+    },
+
+
+    checkQuestion: function() {
+      $('input[type="radio"]').on('change', function() {
+
+        if(Questions[$counter].correct_answer === this.value) {
+          console.log('you guessed right');
+        } else {
+          console.log('you guessed wrong');
+        }
+
+        $question.addClass('animated fadeOut');
+        setTimeout(function() {
+          $question.remove();
+        }, 2000);
+
+        // increment counter
+        $counter++;
+
+      });
+
+
+    },
+
+
+
+
+
+
+
+
+
+
+  } // end game object
+
+
+
+
+
+  game.init();
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ pseudocode.md
@@ -0,0 +1,77 @@
+# steps
+
+1. start game, init()
+
+   - init all questions, rights, wrongs, etc.
+
+
+   - choose a question and display
+     - question has properties (question, answers, correct_anser)
+     - questions stored as array?
+     - iterate over question.length to get one by one ? 
+       - Every time a question is done (timer up, answered right/wrong) you can increment a counter for your current question and check after you increment it: `if (counter == questionArray.length)` then the game ends
+
+2. display question
+
+   - start timer (counts down from 30s)
+
+
+   - check if time < 1, out of time displayRightAnswer()
+     - if last question, endGame()
+     - else, get next question
+   - check if answer is correct, displayCongrats()
+     - if last question, endGame()
+     - else, get next question
+   - else, displaySorry() 
+     - if last question, endGame()
+     - else, get next question
+
+3. endGame()
+
+   - show numRight and numWrong, and unAsnwered
+   - start over/reset button
+
+
+
+
+
+
+
+
+
+counter = 0
+
+then you invoke getQuestion()
+
+-   question  = questions[counter]
+
+-   counter++
+
+-   timer()
+
+    -   checkQuestion()
+
+        -   checkTime()
+
+            - display 'out of time'
+            - if time < 1, display right answer 
+            - getQuestion()
+
+        -   checkAnswer()
+
+            -   if right, rightAnswer()
+
+                - right answer, display congrats
+
+
+                - getQuestion()
+
+            -   else wrongAnswer()
+
+                - wrong answer, display right answer
+                - getQuestion()
+
+        -   checkIfLast()
+
+            - endGame()
+            - getQuestion()
